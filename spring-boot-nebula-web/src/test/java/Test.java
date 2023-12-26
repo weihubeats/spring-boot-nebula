94c2a2875f9fe287efd280e327268beade1e5ce1
weihu <weihu@apache.org>
2023-12-26 15:15:59 +0800

    add test

---
+++ spring-boot-nebula-web/src/test/java/Test.java
@@ -0,0 +1,13 @@
+/**
+ * @author : wh
+ * @date : 2023/12/26 15:13
+ * @description:
+ */
+public class Test {
+    
+    @org.junit.jupiter.api.Test
+    public void test() {
+        System.out.println("hahah");
+        
+    }
+}
