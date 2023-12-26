4bb9c21bcfc9b218de69e139eaf3e23721043a99
weihu <weihu@apache.org>
2023-12-26 17:21:43 +0800

    add test

---
+++ spring-boot-nebula-web/src/test/java/ActionTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+/**
+ * @author : wh
+ * @date : 2023/12/26 15:13
+ * @description:
+ */
+@ExtendWith(MockitoExtension.class)
+public class ActionTest {
+
+    @Test
+    public void test() {
+        System.out.println("hahah");
+        throw new RuntimeException("测试异常");
+        
+    }
+}
---
+++ spring-boot-nebula-web/src/test/java/com/nebula/web/boot/api/NebulaPageTest.java
@@ -0,0 +1,19 @@
+package com.nebula.web.boot.api;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author : wh
+ * @date : 2023/12/26 17:05
+ * @description:
+ */
+public class NebulaPageTest {
+
+    @Test
+    public void testGetTotalCount() {
+        System.out.println("hahah");
+        throw new RuntimeException("测试异常");
+
+    }
+
+}
